4b3caabb42aae78dba0de874b85e5dbdb166927a
harshita <harshitacodes>
2020-05-29 10:34:59 +0530

    updating name of files

--- LIST/bubblesort.py
+++ LIST/bubblesort.py
@@ -8,3 +8,18 @@ for j in range (len(num_list)-1,0,-1):
 	else:
 		list1=num_list
 print("sorted list:",num_list)
+
+
+
+
+
+num_list=[24, 33, 44, 5, 21, 19, 23, 30]
+print("unsorted list:",num_list)
+for j in range (len(num_list)-1,0,-1):
+	for i in range (j):
+		if num_list[i]>num_list[i+1]:
+			num_list[i],num_list[i+1]=num_list[i+1],num_list[i]
+			list2=num_list
+	else:
+		list1=num_list
+print("sorted list:",num_list)
--- LIST/list14@.py
+++
@@ -1,4 +0,0 @@
-length = len(students) # kyunki dono ki same length hai toh jiski bhi length le sakte ho
-counter = 0
-while counter < length:
-	print students[counter] + str(marks[counter])
